dda63e936fca95efa7b8ae3e0bad1f714c860ee3
Guilherme  Ferreira <dev10@circleaceleradora.com.br>
2020-05-12 21:36:36 -0300

    Falta o Delete

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, jsonify, request
-from routes import get_team
+from routes import cadastrar_time, select_time_favorito
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    nome_time = request.args['team_name']
-    return get_team(nome_time)
+@app.route('/cadastrar/time-favorito', methods=['POST'])
+def request_time():
+    # nome_time = request.args['team_name']
+    body = request.get_json()
+    return jsonify(cadastrar_time(body)), 201
+
+
+@app.route('/time-favorito')
+def time_favorito():
+    # nome_time = request.args['team_name']
+    nome = request.args['nome']
+    return jsonify(select_time_favorito(nome)), 200
 
 
 app.run(host="0.0.0.0", port="7000", debug=True)
-
--- routes.py
+++ routes.py
@@ -1,4 +1,37 @@
 import requests as r
+import sqlite3
+
+
+def cadastrar_time(team_name):
+    body = get_team(team_name['nome'])
+    connection = sqlite3.connect('db.db')
+    cursor = connection.cursor()
+    ddl_sql = """
+    CREATE TABLE IF NOT EXISTS TIME(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOME VARCHAR(100),
+        PAIS VARCHAR(50),
+        FUNDACAO INT,
+        ESTADIO VARCHAR(255),
+        CAPACIDADE INT
+    )
+
+    """
+    cursor.execute(ddl_sql)
+    connection.commit()
+
+
+    dml_insert_sql = """
+    INSERT INTO TIME(NOME, PAIS, FUNDACAO, ESTADIO, CAPACIDADE)
+    VALUES(?, ?, ?, ?, ?)
+    """
+
+    cursor.execute(dml_insert_sql, [body['team']['name'], body['team']['country'], 
+                   body['team']['founded'], body['venue']['name'], body['venue']['capacity']])
+    connection.commit()
+    connection.close()
+    return "Time escolhido com sucesso com sucesso !!"
+
 
 def get_team(team_name):
     url = f"https://api-football-beta.p.rapidapi.com/teams?name={team_name}"
@@ -9,4 +42,25 @@ def get_team(team_name):
     }
 
     response = r.get(url, headers=headers).json()
-    return response
+    return response['response'][0]
+
+
+def select_time_favorito(time):
+    connection = sqlite3.connect('db.db')
+    cursor = connection.cursor()
+    ddl_sql = """
+    SELECT NOME, PAIS, FUNDACAO, ESTADIO, CAPACIDADE FROM TIME
+    WHERE NOME = ?
+    """
+    cursor.execute(ddl_sql, [time])
+    time = cursor.fetchone()
+    dic_time = {
+        "nome": time[0],
+        "pais": time[1],
+        "fundacao": time[2],
+        "estadio": time[3],
+        "capacidade": time[4]
+    }
+    connection.commit()
+    connection.close()
+    return dic_time
